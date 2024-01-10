0ff0380d5789e7f8341623ce1d649540f7f97be3
mmarcins <marek.marcinski.external@worldline.com>
2024-01-10 19:48:29 +0100

    Enable async

--- src/main/java/com/marcinski/taskprocessor/TaskProcessorApplication.java
+++ src/main/java/com/marcinski/taskprocessor/TaskProcessorApplication.java
@@ -2,12 +2,13 @@ package com.marcinski.taskprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class TaskProcessorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskProcessorApplication.class, args);
     }
-
 }
